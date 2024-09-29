91ea6e55c21555fff9becf9844e6ac285b607027
Lukácsi Noel Adrián <f1gur3@Lukacsi-MacBook-Air.local>
2024-09-29 10:22:42 +0200

    Set up database to backend connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,27 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import db from '../src/db/db.js'
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-require('dotenv').config();
+
+app.use(cors());
+
+app.use(express.json());
+
 
 const port = process.env.PORT || 2999;
+
+
+try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
 
 app.listen(port, () => {
     console.log("Listening on port: " + port);
 })
+
